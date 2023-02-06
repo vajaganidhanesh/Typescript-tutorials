78d6d9f6f3b9d32f0bd111b9483341bdf4fa0369
vajagani dhanesh <vajaganidhanesh@gmail.com>
2023-02-06 22:02:16 +0530

    objects with features

--- src/arrays.js
+++ src/arrays.js
@@ -1,2 +1,13 @@
 var arr11 = [1, true, "value"];
 console.log(arr11);
+// objects in Typescript
+var TV = {
+    nameTv: "Samsung HD",
+    price: 2300
+};
+// we can configure nullable property in object
+var TvNumber = {
+    Company: 'Panasonic',
+    PriceCompany: 23000
+};
+console.log(TvNumber.Company);
--- src/arrays.ts
+++ src/arrays.ts
@@ -1,2 +1,15 @@
 let arr11: any[] = [1, true, "value"];
 console.log(arr11);
+
+// objects in Typescript
+let TV = {
+  nameTv: "Samsung HD",
+  price: 2300,
+};
+
+// we can configure nullable property in object
+let TvNumber:{Company:string | number,PriceCompany:Number} = {
+    Company:'Panasonic',
+    PriceCompany:23000
+}
+console.log(TvNumber.Company)
