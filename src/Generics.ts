3f0b89f23e867936c19de81a5f910b55ab0e3444
vajagani dhanesh <vajaganidhanesh@gmail.com>
2023-02-14 12:54:46 +0530

    prettier configuration added

--- src/Generics.ts
+++ src/Generics.ts
@@ -1,30 +1,34 @@
-class Demo{
-    public Print<T>(a:T, b:T){
-        console.log(a, b);   
-    }
+class Demo {
+  public Print<T>(a: T, b: T) {
+    console.log(a, b);
+  }
 }
 let obj = new Demo();
-obj.Print<number>(12,20)
-obj.Print<String>('hello','world!...')
+obj.Print<number>(12, 20);
+obj.Print<String>("hello", "world!...");
 
-interface iProduct{
-    Product:string,
-    Price:number,
+interface iProduct {
+  Product: string;
+  Price: number;
 }
 
-interface Empolyee{
-    Name:string,
-    Designation:string,
-    Salary:number
+interface Empolyee {
+  Name: string;
+  Designation: string;
+  Salary: number;
 }
 
-class Details{
-    public PrintDetails<T>(data:T){
-        for(var property in data){
-            console.log(`${property} : ${data[property]}`);
-        }
+class Details {
+  public PrintDetails<T>(data: T) {
+    for (var property in data) {
+      console.log(`${property} : ${data[property]}`);
     }
+  }
 }
 
 let obj1 = new Details();
-obj1.PrintDetails<Empolyee>({Name:'Dhanesh',Designation:'developer',Salary:30000})
+obj1.PrintDetails<Empolyee>({
+  Name: "Dhanesh",
+  Designation: "developer",
+  Salary: 30000,
+});
