d4d082fb8f77bbd3605b432271f9efc8bb771775
vajagani dhanesh <vajaganidhanesh@gmail.com>
2023-02-15 22:07:54 +0530

    Enumeration concept

---
+++ src/Enum.js
@@ -0,0 +1,25 @@
+var Interface;
+(function (Interface) {
+    Interface[Interface["value1"] = 22] = "value1";
+    Interface[Interface["value2"] = 23] = "value2";
+    Interface[Interface["value3"] = 24] = "value3";
+    Interface[Interface["value4"] = 25] = "value4";
+})(Interface || (Interface = {}));
+// console.log(Interface);
+// String formation auto implementation is not possible
+var StringExpression;
+(function (StringExpression) {
+    StringExpression["NotFoung"] = "Page not Found";
+    //   Notfound,
+    StringExpression[StringExpression["ok"] = 33] = "ok";
+    StringExpression[StringExpression["val"] = 34] = "val";
+})(StringExpression || (StringExpression = {}));
+// console.log(StringExpression);
+// Expressions in Enum
+var Expressions;
+(function (Expressions) {
+    Expressions[Expressions["a"] = 10] = "a";
+    Expressions[Expressions["b"] = 20] = "b";
+    Expressions[Expressions["c"] = 30] = "c";
+})(Expressions || (Expressions = {}));
+console.log(Expressions.c);
---
+++ src/Enum.ts
@@ -0,0 +1,27 @@
+enum Interface {
+  value1 = 22,
+  value2,
+  value3,
+  value4,
+}
+
+// console.log(Interface);
+
+// String formation auto implementation is not possible
+enum StringExpression {
+  NotFoung = "Page not Found",
+  //   Notfound,
+  ok = 33,
+  val,
+}
+
+// console.log(StringExpression);
+
+// Expressions in Enum
+enum Expressions {
+  a = 10,
+  b = 20,
+  c = a + b,
+}
+
+console.log(Expressions.c);
